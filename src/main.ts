2179263a895c682abd7d5adde2a5f0a415edce9c
James Zhao <jzha0385@uni.sydney.edu.au>
2024-02-14 03:10:43 +1100

    finalized

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,7 @@
 import { createApp } from 'vue'
-import App from './App.vue'
-
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import App from './App.vue'
 
 const app = createApp(App)
 
