b8347eda087b7275484858a6a8aa5e9478364de6
James Zhao <jzha0385@uni.sydney.edu.au>
2024-02-15 00:55:19 +1100

    firestore initialisation from main.py

--- functions/main.py
+++ functions/main.py
@@ -4,11 +4,12 @@
 
 from firebase_functions import https_fn
 from firebase_admin import initialize_app
+import firebase_admin
+from firebase_admin import firestore
 
-
-
-initialize_app()
-
+# Application Default credentials are automatically created.
+app = firebase_admin.initialize_app()
+db = firestore.client()
 
 @https_fn.on_request()
 def on_request_example(req: https_fn.Request) -> https_fn.Response:
